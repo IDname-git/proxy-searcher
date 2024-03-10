4aa2cf71411aea0fd9d7eb3444571fd80e961837
idname <96003952+IDname-git@users.noreply.github.com>
2024-03-10 17:38:42 +0100

    Updated to V1.1

--- Source code/main.py
+++ Source code/main.py
@@ -5,13 +5,13 @@ import requests
 #Made by IDname 
 #DC: IDname
 
-print('Version 1')
+print('Version 1.1 Made by IDname')
 print('█▀█ █▀█ █▀█ ▀▄▀ █▄█   █▀ █▀▀ █▀█ ▄▀█ █▀█ █▀▀ █▀█   █░█ ▄█')
 print('█▀▀ █▀▄ █▄█ █░█ ░█░   ▄█ █▄▄ █▀▄ █▀█ █▀▀ ██▄ █▀▄   ▀▄▀ ░█')
 
 
 #Default settings
-anonymity= 'elite'
+ProxyAnonymity= 'elite'
 ProxyRequestoutputsave = 'y'
 Proxyregion = 'all'
 ProxyTimeout = 100
@@ -20,18 +20,20 @@ ProxyProtocol= 'http'
 #Options
 print('Recommended: de, en , pl , fr , be , in , it , hr , at , se, rs , es , to , tr , all')
 print('There are many more. Just use the ISO-3166-1 standard')
-Proxyregion = input ('Which Region do you want to use ? (Enter to use all)')
+Proxyregion = input ('Which Region do you want to use ? Enter to use all')
 print('')
 print('Recommended: 100 Curently:',ProxyTimeout)
-ProxyTimeout = input ('What should be the Proxy timeout ? Enter to use Recommended')
+ProxyTimeout = input ('What should be the Proxy timeout ? Enter to use 100 (Recommended)')
 print('')
 print('Aviable: http , socks4 , socks5')
 ProxyProtocol = input('Which Protocol do you want to use ? Enter to use http')
+print('Aviable: elite , anonymous , transparent or all')
+ProxyAnonymity = input('Which Anonymity do you want to use ? Enter for elite (Recommended)')
 print('Checking Aviable Proxies ...')
 
 #Request
 ProxyAviable = requests.get('https://api.proxyscrape.com/v2/?request=proxyinfo&simplified=true')
-ProxyRequestURL = 'https://api.proxyscrape.com/v2/?request=displayproxies&protocol='+ProxyProtocol+'&timeout='+ProxyTimeout+'&country='+Proxyregion+'&ssl=all&anonymity='+anonymity+''
+ProxyRequestURL = 'https://api.proxyscrape.com/v2/?request=displayproxies&protocol='+ProxyProtocol+'&timeout='+ProxyTimeout+'&country='+Proxyregion+'&ssl=all&anonymity='+ProxyAnonymity+''
 API_Data = ProxyAviable.json()
 for key in API_Data:{
     print(key,":", API_Data[key])
@@ -42,7 +44,7 @@ ProxyRequestoutput = requests.get(ProxyRequestURL)
 print('Protocol:'+ProxyProtocol+' | Timeout:'+ProxyTimeout+' | Country: '+ Proxyregion +' | Anonymity: Elite' )
 time.sleep(1.0)
 if ProxyRequestoutput.text == '':
-         print('No Proxies aviable')
+         print('No Proxies aviable or Wrong input')
          print('Try a higher Proxy Timeout')
 else:
          print(ProxyRequestoutput.text)
